ab93560f14fbb2a5c02ec5b3a0afbefbbc1356d1
jakelizzI <jakelizzI@gmail.com>
2019-10-18 00:10:18 +0900

    背景の透過:

--- src/index.js
+++ src/index.js
@@ -1,27 +1,29 @@
 'use strict';
 
-import electron from 'electron';
+const electron = require('electron');
+const localShortcut = require('electron-localshortcut');
 
-const app = electron.app;
-const BrowserWindow = electron.BrowserWindow;
+let app = electron.app;
+let BrowserWindow = electron.BrowserWindow;
+
+let mainWindow;
 
 app.on('ready', () => {
-  import Screen from 'screen';
 
-  const size = Screen.getPrimaryDisplay().size;
-  const mainWindow = new BrowserWindow({
-    left: 0,
-    top: 0,
-    width: size.width,
-    height: size.height,
-    frame: false,
-    show: true,
-    transparent: true,
-    resizable: false,
-    alwaysOnTop: true,
+  mainWindow = new BrowserWindow({
+    width: 800,
+    height: 600,
+    'frame': false,
+    'fullscreen': true,
+    'transparent': true
   })
+  mainWindow.loadURL('file://' + __dirname + '/index.html');
+
+  localShortcut.register(mainWindow, 'Ctrl+W', () => {
+    mainWindow = null;
+  });
 
   mainWindow.on('closed', () => {
     mainWindow = null;
-  })
+  });
 });
